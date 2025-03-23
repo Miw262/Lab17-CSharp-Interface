fb6807470689e5ef1121503572acece9ff24d8d6
Miw262 <chauat246@gmail.com>
2025-03-23 15:52:40 +0700

    66030262 พีรวิชญ์ บุญฤทธิ์

---
+++ Lab17_Ex04/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+interface IMovable
+{
+    void Move(); // ✅ ลบ public ออก
+}
+interface ISpeakable
+{
+    void Speak(); // ✅ ลบ public ออก
+}
+
+abstract class Animal : IMovable, ISpeakable
+{
+    public abstract void Move(); // ✅ public อยู่ข้างหน้า abstract
+    public abstract void Speak(); // ✅ ต้องมีเมธอดนี้เพื่อให้ Animal ใช้ ISpeakable ได้
+}
+
+class Dog : Animal
+{
+    public override void Move()
+    {
+        Console.WriteLine("Dog moves by running on the ground");
+    }
+
+    public override void Speak()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+class Fish : Animal
+{
+    public override void Move()
+    {
+        Console.WriteLine("Fish moves by swimming in the water");
+    }
+
+    public override void Speak()
+    {
+        Console.WriteLine("Fish does not make sound");
+    }
+}
+
+class Bird : Animal
+{
+    public override void Move()
+    {
+        Console.WriteLine("Bird moves by flying in the air");
+    }
+
+    public override void Speak()
+    {
+        Console.WriteLine("Bird chirps");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal[] animals = new Animal[3];
+        animals[0] = new Dog();
+        animals[1] = new Bird();
+        animals[2] = new Fish();
+
+        foreach (var animal in animals)
+        {
+            animal.Move();
+            animal.Speak(); // ✅ เรียก Speak() ได้แล้ว
+        }
+    }
+}
---
+++ Lab17_Ex06/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+interface IRemoteControl
+{
+    void TurnOn();  // ✅ ลบ public ออก
+    void TurnOff();
+    void ChannelUp();
+    void ChannelDown();
+}
+
+abstract class PowerAppliance
+{
+    public bool PowerStatus;
+    public int Wattage;
+}
+
+// ✅ แก้ไขให้สืบทอด IRemoteControl
+class Television : PowerAppliance, IRemoteControl
+{
+    public int Channel { get; set; }
+
+    public virtual void TurnOn() { Console.WriteLine("TV Turn on"); PowerStatus = true; }
+    public virtual void TurnOff() { Console.WriteLine("TV Turn off"); PowerStatus = false; }
+    public virtual void ChannelUp() { Console.WriteLine("TV Channel up"); }
+    public virtual void ChannelDown() { Console.WriteLine("TV Channel down"); }
+}
+
+// ✅ แก้ไขให้สืบทอด IRemoteControl
+class Lamp : PowerAppliance, IRemoteControl
+{
+    public virtual void TurnOn() { Console.WriteLine("Lamp Turn on"); PowerStatus = true; }
+    public virtual void TurnOff() { Console.WriteLine("Lamp Turn off"); PowerStatus = false; }
+    public virtual void ChannelUp() { Console.WriteLine("Lamp cannot change channel"); }
+    public virtual void ChannelDown() { Console.WriteLine("Lamp cannot change channel"); }
+}
+
+class SonyTV : Television
+{
+    public override void TurnOn() { Console.WriteLine("Sony TV Turn on"); PowerStatus = true; }
+    public override void TurnOff() { Console.WriteLine("Sony TV Turn off"); PowerStatus = false; }
+}
+
+class DesktopLamp : Lamp
+{
+    public override void TurnOn() { Console.WriteLine("Desktop Lamp Turn on"); PowerStatus = true; }
+    public override void TurnOff() { Console.WriteLine("Desktop Lamp Turn off"); PowerStatus = false; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // ✅ ใช้ IRemoteControl เป็นประเภทตัวแปรเพื่อให้เรียกเมธอดได้
+        IRemoteControl myTV = new SonyTV();
+        myTV.TurnOn();
+        myTV.ChannelUp();
+        myTV.ChannelDown();
+        myTV.TurnOff();
+
+        IRemoteControl myLamp = new DesktopLamp();
+        myLamp.TurnOn();
+        myLamp.ChannelUp();
+        myLamp.ChannelDown();
+        myLamp.TurnOff();
+    }
+}
